038301680de26b9cd10a3bad358ce278df9aece2
Riley Shott <rileyshott@gmail.com>
2013-06-09 23:04:39 -0700

    Added google_drive_helper.rb script

---
+++ google_drive_helper.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+#####################################################################
+#  Name: Google Drive Helper
+#  Author: Riley Shott
+#  Date: May 20, 2013
+#  Version: 0.1.0
+#  Description: Places the files Google Drive needs to modify folder icons,
+#               which prevents it from asking for an Administrator password.
+#####################################################################
+
+require 'fileutils'
+
+icon_helper        = '/Applications/Google Drive.app/Contents/Resources/Google Drive Icon Helper'
+icon_helper_path   = '/Library/PrivilegedHelperTools'
+icon_helper_script = "#{icon_helper_path}/Google Drive Icon Helper"
+
+exit 0 if File.exists? icon_helper_script
+
+if File.exists? icon_helper
+  begin
+    FileUtils.cp icon_helper, icon_helper_path
+    FileUtils.chmod 06755, icon_helper_script
+    FileUtils.chown 'root', 'procmod', icon_helper_script
+  rescue Exception => e
+    message = 'An error occured:' + "\n\n" + "Error Message: " + e.message + "\n" + "Backtrace: " + e.backtrace.inspect
+    puts message
+  end
+else
+  puts "#{icon_helper} doesn't seem to exist."
+  exit 1
+end
+
+exit 0
