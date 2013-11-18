00e92a20020ae09204b2a8465d295886767d2011
Riley Shott <rileyshott@gmail.com>
2013-11-17 22:35:32 -0800

    Added a parameter flag to toggle archiving functionality Beefed up logic and error handling for cleanup method

--- drupal_db_dump.rb
+++ drupal_db_dump.rb
@@ -5,18 +5,28 @@
 # Title: Drupal Database Backup Utility
 # Created: Nov 04, 2013
 # Author: rshott@sfu.ca
-# Version: 0.5
+# Version: 0.7
 ############################################################
 
 ### GLOBAL VARIABLES ###
 
-DAY  = `/bin/date +\%d`.chomp.to_i
-DATE = `/bin/date +\%Y-\%m-\%d`.chomp
+DAY   = `date +\%d`.chomp.to_i
+MONTH = `date +\%m`.chomp.to_i
+YEAR  = `date +\%Y`.chomp.to_i
+DATE  = "#{YEAR}-#{MONTH}-#{DAY}"
+USAGE = <<EOF
+Usage: #{__FILE__} <drupal_sites_location> <dump_path> [archive]
+
+Example: #{__FILE__} /var/www/drupal/drupal-7x/sites /var/www/drupal/database_dumps archive
+Example: #{__FILE__} /var/www/drupal/drupal-7x/sites /var/www/drupal/database_dumps
+
+Options:
+  archive         If specified, will purge all but the last backup for the previous month when a new month begins.
+EOF
 
 ### LIBRARIES ###
 
 require 'fileutils'
-require 'date'
 
 ### CLASSES ###
 
@@ -41,7 +51,7 @@ class Drupal
         File.open("#{DATE}_#{@site_name}/checksum.txt", 'w') { |f| f.write "#{@site_name}    #{md5}" }
         if system("tar cvfz #{DATE}_#{@site_name}.tar.gz #{DATE}_#{@site_name} && rm -rf #{DATE}_#{@site_name}")
           # Checking if we need to start archiving
-          cleanup if DAY > 28
+          cleanup if DAY > 28 && ARGV[2].eql?('archive')
         end
       end
     end
@@ -52,11 +62,28 @@ class Drupal
     def cleanup
       Dir.chdir("#{@dump_path}/#{@site_name}")
       files = Dir.glob("*_#{@site_name}.tar.gz").sort
-      if files.collect { |x| x.split('-')[1] }.uniq.length > 1
-        FileUtils.mkdir('monthly_archives', :mode => 0700 ) unless File.directory?('monthly_archives')
-        latest = files.pop(2)[0]
-        FileUtils.mv(latest, 'monthly_archives')
-        FileUtils.rm(files, :force => true)
+      month_count = files.collect { |x| x.split('-')[1] }.uniq.length
+      if month_count == 2
+        begin
+          year, month = last_backup_period
+          FileUtils.mkdir('monthly_archives', :mode => 0700 ) unless File.directory?('monthly_archives')
+          last_months_files = Dir.glob("#{year}-#{month}-*_#{@site_name}.tar.gz").sort
+          FileUtils.mv(last_months_files.pop, 'monthly_archives')
+          FileUtils.rm(last_months_files, :force => true)
+        rescue Exception => e
+          message = 'An error occured while trying to archive for #{@site_name}:' + "\n\n" + "Error Message: " + e.message + "\n" + "Backtrace: " + e.backtrace.inspect
+          puts message
+        end
+      elsif month_count >= 3
+        puts "Backups span more than two months for #{@site_name}. Please manually resolve this if you want this functionality to work."
+      end
+    end
+
+    def last_backup_period
+      if MONTH == 1
+        return YEAR - 1, 12
+      else
+        return YEAR, MONTH - 1
       end
     end
 
@@ -64,29 +91,31 @@ end
 
 ### SANITY CHECKS ###
 
-if ARGV.length != 2 or ARGV[0].eql?('-h')
-  puts "Usage: #{__FILE__} <drupal_sites_location> <dump_path>"
-  puts "Example: #{__FILE__} /var/www/drupal/drupal-7x/sites /var/www/drupal/database_dumps"
+if ARGV[0].eql?('-h') || ARGV[0].eql?('--help')
+  puts USAGE
+  exit 0
+end
+
+case ARGV.length
+when 2
+when 3
+else
+  puts USAGE
   exit 1
 end
 
-unless File.directory?(ARGV[0])
-  puts "The drupal sites location needs to be an existing directory - #{ARGV[0]}"
-  exit 1
-end
-
-unless File.directory?(ARGV[1])
-  puts "The dump location needs to be an existing directory - #{ARGV[1]}"
-  exit 1
-end
+error_messages = []
+error_messages << "The drupal sites location needs to be an existing directory - #{ARGV[0]}" unless File.directory?(ARGV[0])
+error_messages << "The dump location needs to be an existing directory - #{ARGV[1]}" unless File.directory?(ARGV[1])
+error_messages << "The third parameter either needs to be 'archive' or not specified at all - #{ARGV[2]}" unless ARGV[2].eql?('archive') or ARGV[2].nil?
 
 check = {}
-error_messages = []
 
 check['drush']   = system("which drush &> /dev/null")
 check['openssl'] = system("which openssl &> /dev/null")
 check['tar']     = system("which tar &> /dev/null")
 check['rm']      = system("which rm &> /dev/null")
+check['date']    = system("which date &> /dev/null")
 check.each do |key, value|
     error_messages << "This script requires #{key}. Please ensure it's installed and in your PATH." if value == false
 end
