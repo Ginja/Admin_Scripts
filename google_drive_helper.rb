bb3b0b5561f4707944a2994428a75b6be6ddf6a9
Riley Shott <rileyshott+git@gmail.com>
2014-11-06 01:00:30 -0800

    Updated google_drive_helper.rb
    Added root check
    Now checking both known paths for the Helper binary

--- google_drive_helper.rb
+++ google_drive_helper.rb
@@ -4,30 +4,39 @@
 #  Name: Google Drive Helper
 #  Author: Riley Shott
 #  Date: May 20, 2013
-#  Version: 0.1.0
+#  Version: 0.2.0
 #  Description: Places the files Google Drive needs to modify folder icons,
 #               which prevents it from asking for an Administrator password.
 #####################################################################
 
 require 'fileutils'
 
-icon_helper        = '/Applications/Google Drive.app/Contents/Helpers/Google Drive Icon Helper'
+APP_PATH = '/Applications/Google Drive.app/Contents'
+
+unless Process.euid == 0
+  puts 'This must be run as root.'
+  exit 1
+end
+
+# Check both paths for the Helper binary (newer, older) 
+# Thanks to Dan Keller (https://github.com/dankeller) for informing me the path had changed
+icon_helper = ["#{APP_PATH}/Helpers/Google Drive Icon Helper", "#{APP_PATH}/Resources/Google Drive Icon Helper"].collect do |file|
+  file if File.exist? file
+end.compact.first
 icon_helper_path   = '/Library/PrivilegedHelperTools'
 icon_helper_script = "#{icon_helper_path}/Google Drive Icon Helper"
 
-exit 0 if File.exists? icon_helper_script
-
-if File.exists? icon_helper
+unless icon_helper.nil?
   begin
-    FileUtils.cp icon_helper, icon_helper_path
+    FileUtils.cp icon_helper, icon_helper_path, :preserve => true
     FileUtils.chmod 06755, icon_helper_script
     FileUtils.chown 'root', 'procmod', icon_helper_script
-  rescue Exception => e
+  rescue => e
     message = 'An error occured:' + "\n\n" + "Error Message: " + e.message + "\n" + "Backtrace: " + e.backtrace.inspect
     puts message
   end
 else
-  puts "#{icon_helper} doesn't seem to exist."
+  puts "Could not find the 'Google Drive Icon Helper' binary."
   exit 1
 end
 
